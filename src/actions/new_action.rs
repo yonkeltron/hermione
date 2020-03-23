51927affe2f658b846035033890cea72569810eb
ehila <eggfoobar@gmail.com>
2020-03-23 04:17:36 -0400

    Changed manifest id to be an argument you can pass in
    Signed-off-by: ehila <eggfoobar@gmail.com>

--- src/actions/new_action.rs
+++ src/actions/new_action.rs
@@ -8,12 +8,13 @@ use crate::scaffold::Scaffold;
 /// New Action scaffolds a Hermione package directory with a `hermione.yml` file and a couple of sample files.
 pub struct NewAction {
     pub package_name: String,
+    pub package_id: String,
 }
 
 impl Action for NewAction {
     fn execute(self, package_service: PackageService) -> Result<()> {
         info!(package_service.logger, "Initialized");
-        let scaffold = Scaffold::new(&self.package_name);
+        let scaffold = Scaffold::new(&self.package_name, &self.package_id);
         scaffold.create_package(&package_service.logger)
     }
 }
